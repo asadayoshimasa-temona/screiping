452cd1b6d195984a704a594b9a6427dc6460e716
asadayoshimasa-temona <asada.yoshimasa@temona.co.jp>
2023-11-13 17:47:11 +0900

    コメントアウト整理

--- yahoo_py/create_category_index_urls.py
+++ yahoo_py/create_category_index_urls.py
@@ -16,10 +16,8 @@ options = Options()
 options.add_argument('--headless')  # ヘッドレスモードで実行。
 options.add_argument('--no-sandbox')  # OSのセキュリティモデルをバイパス。
 options.add_argument('--disable-dev-shm-usage')
-# options.add_argument('--headless')  # ヘッドレスモードで実行する場合など
 
 # WebDriverのServiceオブジェクトを作成
-# 実際のchromedriverのパスに置き換えてください
 service = Service(executable_path='/usr/bin/chromedriver')
 
 # WebDriverを起動
@@ -29,12 +27,10 @@ driver = webdriver.Chrome(service=service, options=options)
 url = 'https://shopping.yahoo.co.jp/category/2496/list?p=&area=13&astk=&first=1&ss_first=1&ts=1699412102&mcr=b80fa60dde8057ac1d85b8b4167e0ffc&tab_ex=commerce&sretry=1&sc_i=shp_pc_search_searchBox_2&sretry=1'
 driver.get(url)
 
-
 response = requests.get(url)
 soup = BeautifulSoup(response.text, 'html.parser')
 search_results_count = soup.find(class_='SearchResultsDisplayOptions_SearchResultsDisplayOptions__count__WBsPf')
 # print(search_results_count.get_text().replace(",", "").replace("件", ""))
-
 
 wait = WebDriverWait(driver, 10)
 element = wait.until(EC.presence_of_element_located((By.CLASS_NAME, 'SearchBox__optionTrigger--filtered')))
@@ -48,7 +44,6 @@ modal = wait.until(EC.presence_of_element_located((By.CLASS_NAME, 'SearchBox__fi
 # selectボックスの値を取得
 select_values = [option.get_attribute('value') for option in modal.find_elements(By.TAG_NAME, 'option')]
 
-
 numeric_select_values = []
 for value in select_values:
     try:
@@ -56,12 +51,12 @@ for value in select_values:
         numeric_value = int(value)
         numeric_select_values.append(numeric_value)
     except ValueError:
-        # 変換できない場合は無視するか、適切な処理を行う
+        # 変換できない場合は無視
+        # 現状全てのカテゴリに対してValueがないのでそれを無視するための記述
         print(f"Value '{value}' cannot be converted to an integer.")
 
 url_template = 'https://shopping.yahoo.co.jp/category/{}/list/'
 category_urls = [url_template.format(value) for value in numeric_select_values]
-print(category_urls)
 
 with open('shop_category_index_url.txt', 'w') as file:
     for category_url in category_urls:
